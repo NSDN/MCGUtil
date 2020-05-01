bf19671efa3ed19a6ff7a0695f9104528b80471b
drzzm32 <drzzm32@gmail.com>
2020-05-01 16:00:22 +0800

    update

--- src/main/java/mcg/util/event/ClientTickHandler.java
+++ src/main/java/mcg/util/event/ClientTickHandler.java
@@ -1,12 +1,7 @@
 package mcg.util.event;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.lang.reflect.Field;
 
 /**
  * Created by drzzm32 on 2020.3.22.
@@ -20,38 +15,8 @@ public class ClientTickHandler {
         return instance;
     }
 
-    public static Class<?> EntityMCGBoat;
-    public static Field jump;
-
-    public ClientTickHandler() {
-        try {
-            EntityMCGBoat = Class.forName("moe.gensoukyo.mcgproject.common.entity.EntityMCGBoat");
-            jump = EntityMCGBoat.getDeclaredField("jump");
-            jump.setAccessible(true);
-        } catch (Exception ignored) {
-            EntityMCGBoat = null;
-            jump = null;
-        }
-    }
-
-    public void setJump(Entity entity, float val) {
-        try {
-            jump.set(entity, val);
-        } catch (Exception ignored) { }
-    }
-
     @SubscribeEvent
     public void tick(TickEvent.ClientTickEvent event) {
-        if (EntityMCGBoat == null || jump == null)
-            return;
 
-        EntityPlayer player = Minecraft.getMinecraft().player;
-        if (player == null) return;
-
-        Entity entity = player.getRidingEntity();
-        if (EntityMCGBoat.isInstance(entity)) {
-            entity.stepHeight = 0.5F;
-            setJump(entity, 0.0F);
-        }
     }
 }
--- src/main/java/mcg/util/event/EventRegister.java
+++ src/main/java/mcg/util/event/EventRegister.java
@@ -2,6 +2,8 @@ package mcg.util.event;
 
 import net.minecraftforge.common.MinecraftForge;
 
+import java.awt.*;
+
 /**
  * Created by drzzm32 on 2020.3.22.
  */
@@ -16,6 +18,9 @@ public class EventRegister {
 
     public static void registerClient() {
         MinecraftForge.EVENT_BUS.register(ClientTickHandler.instance());
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        for (Font f : ge.getAllFonts())
+            System.out.println(f.getName());
     }
 
 }
